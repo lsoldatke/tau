7750018ba561f6b0e7c805fe1aa2d91eb6911460
lukas <s24729@pjwstk.edu.pl>
2024-12-20 13:27:44 +0100

    Added sql db initialization scripts

---
+++ lab4/src/main/resources/data.sql
@@ -0,0 +1,61 @@
+-- Data generated with www.mockaroo.com
+insert into product (id, name, price, quantity)
+values (1, 'Wine - Mas Chicet Rose, Vintage', 30.95, 83);
+insert into product (id, name, price, quantity)
+values (2, 'Stock - Veal, Brown', 174.59, 7);
+insert into product (id, name, price, quantity)
+values (3, 'Chicken - Livers', 10.63, 15);
+insert into product (id, name, price, quantity)
+values (4, 'Thyme - Fresh', 135.89, 27);
+insert into product (id, name, price, quantity)
+values (5, 'Ecolab - Power Fusion', 100.01, 63);
+insert into product (id, name, price, quantity)
+values (6, 'Sterno - Chafing Dish Fuel', 140.97, 27);
+insert into product (id, name, price, quantity)
+values (7, 'Carbonated Water - Strawberry', 96.73, 5);
+insert into product (id, name, price, quantity)
+values (8, 'Artichokes - Knobless, White', 37.24, 97);
+insert into product (id, name, price, quantity)
+values (9, 'Sponge Cake Mix - Chocolate', 11.8, 14);
+insert into product (id, name, price, quantity)
+values (10, 'Cabbage - Nappa', 39.81, 18);
+insert into product (id, name, price, quantity)
+values (11, 'Steampan - Foil', 100.65, 57);
+insert into product (id, name, price, quantity)
+values (12, 'Lamb - Leg, Bone In', 69.87, 18);
+insert into product (id, name, price, quantity)
+values (13, 'Wine - Barossa Valley Estate', 155.06, 66);
+insert into product (id, name, price, quantity)
+values (14, 'Curry Paste - Green Masala', 194.66, 50);
+insert into product (id, name, price, quantity)
+values (15, 'Bread - French Stick', 139.48, 32);
+insert into product (id, name, price, quantity)
+values (16, 'Ice - Clear, 300 Lb For Carving', 196.03, 94);
+insert into product (id, name, price, quantity)
+values (17, 'Artichokes - Jerusalem', 110.6, 45);
+insert into product (id, name, price, quantity)
+values (18, 'Crackers - Soda / Saltins', 132.45, 92);
+insert into product (id, name, price, quantity)
+values (19, 'Napkin - Beverage 1 Ply', 103.38, 10);
+insert into product (id, name, price, quantity)
+values (20, 'Juice - Orangina', 156.39, 55);
+insert into product (id, name, price, quantity)
+values (21, 'Chinese Lemon Pork', 41.72, 76);
+insert into product (id, name, price, quantity)
+values (22, 'Pasta - Cheese / Spinach Bauletti', 166.56, 2);
+insert into product (id, name, price, quantity)
+values (23, 'Milk - 2% 250 Ml', 172.27, 100);
+insert into product (id, name, price, quantity)
+values (24, 'Sprouts Dikon', 111.57, 18);
+insert into product (id, name, price, quantity)
+values (25, 'Beans - Green', 21.72, 61);
+insert into product (id, name, price, quantity)
+values (26, 'Stock - Beef, White', 136.93, 68);
+insert into product (id, name, price, quantity)
+values (27, 'Roe - White Fish', 92.25, 64);
+insert into product (id, name, price, quantity)
+values (28, 'Oil - Shortening,liqud, Fry', 137.5, 87);
+insert into product (id, name, price, quantity)
+values (29, 'Truffle Shells - Semi - Sweet', 123.94, 55);
+insert into product (id, name, price, quantity)
+values (30, 'Maintenance Removal Charge', 55.27, 25);
---
+++ lab4/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+-- Data generated with www.mockaroo.com
+create table product
+(
+    id       BIGINT PRIMARY KEY,
+    name     VARCHAR(255),
+    price    DECIMAL(5, 2),
+    quantity INT
+);
