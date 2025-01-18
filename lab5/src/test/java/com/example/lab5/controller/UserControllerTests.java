06a39f46cff8707c6768f361b0714318a8998cfb
lukas <s24729@pjwstk.edu.pl>
2025-01-19 00:46:07 +0100

    Tests fixes

--- lab5/src/test/java/com/example/lab5/controller/UserControllerTests.java
+++ lab5/src/test/java/com/example/lab5/controller/UserControllerTests.java
@@ -2,6 +2,8 @@ package com.example.lab5.controller;
 
 import com.example.lab5.model.User;
 import com.example.lab5.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.List;
 
@@ -22,6 +25,8 @@ public class UserControllerTests {
     private MockMvc mockMvc;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setup() {
@@ -59,44 +64,72 @@ public class UserControllerTests {
     @Test
     void createsUser() throws Exception {
         User correctUser = new User(null, "Lisa Carek", "lisacarek@example.com");
+        String correctUserJson = objectMapper.writeValueAsString(correctUser);
 
-        mockMvc.perform(post("/users"))
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(correctUserJson))
                 .andExpect(status().isCreated())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.length()").value(1))
                 .andExpect(jsonPath("$.name").value("Lisa Carek"))
                 .andExpect(jsonPath("$.email").value("lisacarek@example.com"));
 
         User incorrectUser = new User(null, null, "annfoster@example.com");
+        String incorrectUserJson = objectMapper.writeValueAsString(incorrectUser);
 
-        mockMvc.perform(post("/users"))
-                .andExpect(status().isBadRequest())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.length()").value(0));
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(incorrectUserJson))
+                .andExpect(status().isBadRequest());
     }
 
     @Test
     void updatesUser() throws Exception {
-        User userUpdate = new User(null, "John Smith", "johnsmith@example.com");
+        User user = new User(null, "Adam Green", "adamgreen@example.com");
+        String userJson = objectMapper.writeValueAsString(user);
 
-        mockMvc.perform(put("/users/1"))
+        MvcResult result = mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userJson))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("Adam Green"))
+                .andExpect(jsonPath("$.email").value("adamgreen@example.com"))
+                .andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        Long userId = JsonPath.parse(response).read("$.id", Long.class);
+
+        User userUpdate = new User(null, "John Smith", "johnsmith@example.com");
+        String userUpdateJson = objectMapper.writeValueAsString(userUpdate);
+
+        mockMvc.perform(put("/users/" + userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userUpdateJson))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.length()").value(1))
                 .andExpect(jsonPath("$.name").value("John Smith"))
                 .andExpect(jsonPath("$.email").value("johnsmith@example.com"));
     }
 
     @Test
     void deletesUser() throws Exception {
-        mockMvc.perform(delete("/users/2"))
-                .andExpect(status().isNoContent())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.length()").value(0));
+        User user = new User(null, "Adam Walker", "adamwalker@example.com");
+        String userJson = objectMapper.writeValueAsString(user);
 
-        mockMvc.perform(get("/users/2"))
-                .andExpect(status().isNotFound())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.length()").value(0));
+        MvcResult result = mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userJson))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("Adam Walker"))
+                .andExpect(jsonPath("$.email").value("adamwalker@example.com"))
+                .andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        Long userId = JsonPath.parse(response).read("$.id", Long.class);
+
+        mockMvc.perform(delete("/users/" + userId))
+                .andExpect(status().isNoContent());
+
+        mockMvc.perform(get("/users/" + userId))
+                .andExpect(status().isNotFound());
     }
 }
